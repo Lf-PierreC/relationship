7f31874f56c2121bbcb9bdbe7e257826167ad0f8
Lf-PierreC <lfpierre.carvalho@gmail.com>
2025-09-05 18:19:47 -0300

    fix momentos

--- src/components/pages/Momentos.js
+++ src/components/pages/Momentos.js
@@ -5,10 +5,21 @@ import styles from './Momentos.module.css';
 function Momentos() {
   return (
     <div>
-        <Header/>
-        <div className={styles.container_qrcode}>
-            <img src="/images/qrcode_6meses.png" alt="Momentos" className={styles.qrcode} />
-        </div>
+      <Header/>
+      <div className={styles.container_qrcode}>
+        <a 
+          href="https://youtu.be/-_rsnrywox0?si=E6eHeV99RJ2ObeIT"  // 🔗 coloque aqui o link do QR Code
+          target="_blank"             // abre em nova aba
+          rel="noopener noreferrer"   // segurança
+        >
+          <img 
+            src="/images/qrcode_6meses.png" 
+            alt="Momentos" 
+            className={styles.qrcode} 
+          />
+        </a>
+      </div>
+      <p className={styles.descricao}>Clique ou leia o QR Code</p>
     </div>
   );
 }
